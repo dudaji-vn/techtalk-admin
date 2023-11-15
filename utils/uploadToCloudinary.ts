0a8086c118559ecf289404c6d73b95b6fd6aa411
Linh <hoquanglinh2710@gmail.com>
2023-11-15 22:05:50 +0700

    Add upload image to cloudinary

---
+++ utils/uploadToCloudinary.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+export interface ICloudinaryResponse {
+  url: string;
+  type: string;
+}
+export const uploadToCloudinary = async (files: File[]) => {
+  const uploadFiles = files.map(async (file) => {
+    return new Promise<ICloudinaryResponse>((resolve, reject) => {
+      const formData = new FormData();
+      formData.append(`file`, file);
+      formData.append("upload_preset", "guq1cwrf");
+      formData.append("cloud_name", "hoquanglinh");
+
+      axios
+        .post<ICloudinaryResponse>(
+          "https://api.cloudinary.com/v1_1/hoquanglinh/image/upload",
+          formData
+        )
+        .then((response) => {
+          resolve({
+            url: response.data.url,
+            type: "image",
+          });
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+
+  try {
+    const imgArr = await Promise.all(uploadFiles);
+    return imgArr;
+  } catch (error) {
+    console.log(error);
+  }
+};
