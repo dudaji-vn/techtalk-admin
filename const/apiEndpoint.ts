44e964f00a321d9707abfa3b690703fc61ed3549
Linh <hoquanglinh2710@gmail.com>
2023-11-15 22:12:28 +0700

    Add constant

---
+++ const/apiEndpoint.ts
@@ -0,0 +1,14 @@
+export const baseApiUrl = "https://r5200nc8-5000.asse.devtunnels.ms/api";
+export const authEndpoint = {
+  login: "auth/adminLogin",
+};
+
+export const lectureEndpoint = {
+  getAll: "lecture/all",
+  addOrUpdateLecture: "lecture/addOrUpdateLecture",
+};
+
+export const vocabularyEndpoint = {
+  getAllVocabularyByLectureId: "vocabulary/getAllVocabulariesByLectureId",
+  addOrUpdateVocabulary: "vocabulary/addOrUpdateVocabulary",
+};
