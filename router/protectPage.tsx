b78ed1d3b322cc3c73295d34c190ddc903ae00f8
Linh <hoquanglinh2710@gmail.com>
2023-11-17 08:44:57 +0700

    Add ProtectPage for authentication

---
+++ router/protectPage.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+import { keyStorage } from "../const/keyStorage";
+
+export default function protectPage(Component: any) {
+  return function ProtectPage(props: any) {
+    const auth = localStorage.getItem(keyStorage.accessToken);
+
+    useEffect(() => {
+      if (!auth) {
+        return redirect("/auth/signin");
+      }
+    }, []);
+
+    if (!auth) {
+      return null;
+    }
+
+    return <Component {...props} />;
+  };
+}
