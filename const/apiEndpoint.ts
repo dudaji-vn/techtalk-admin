411c7ceb3f5c92050ff631af7de24a8ebbd0ec08
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-01 10:58:47 +0700

    Update api endpoint

--- const/apiEndpoint.ts
+++ const/apiEndpoint.ts
@@ -1,14 +1,14 @@
-export const baseApiUrl = "https://techtalk-02ik.onrender.com/api";
+export const baseApiUrl = 'http://localhost:5000/api';
 export const authEndpoint = {
-  login: "auth/adminLogin",
+  login: 'auth/adminLogin',
 };
 
 export const lectureEndpoint = {
-  getAll: "lecture/allForAdmin",
-  addOrUpdateLecture: "lecture/addOrUpdateLecture",
+  getAll: 'lecture/allForAdmin',
+  addOrUpdateLecture: 'lecture/addOrUpdateLecture',
 };
 
 export const vocabularyEndpoint = {
-  getAllVocabularyByLectureId: "vocabulary/getAllVocabulariesByLectureId",
-  addOrUpdateVocabulary: "vocabulary/addOrUpdateVocabulary",
+  getAllVocabularyByLectureId: 'vocabulary/getAllVocabulariesByLectureId',
+  addOrUpdateVocabulary: 'vocabulary/addOrUpdateVocabulary',
 };
