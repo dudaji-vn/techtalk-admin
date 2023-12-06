0ef70c861dbd33e81f0705002eee11d0870ea50f
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-06 10:59:20 +0700

    Update UI header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,16 @@
-import { keyStorage } from "@/const/keyStorage";
-import { useRouter } from "next/navigation";
-import LogoutIcon from "../Icons/LogoutIcon";
+import { keyStorage } from '@/const/keyStorage';
+import { useRouter } from 'next/navigation';
+import LogoutIcon from '../Icons/LogoutIcon';
+import Typography from '../Typo';
 const Header = () => {
   const router = useRouter();
   const handleLogout = () => {
     localStorage.removeItem(keyStorage.accessToken);
-    router.push("/auth/signin");
+    router.push('/auth/signin');
   };
   return (
-    <header className="sticky top-0 justify-end z-999 flex w-full bg-white pt-4">
+    <header className="sticky top-0 z-999 flex justify-between bg-white p-4">
+      <Typography type="semi-bold">Admin page</Typography>
       <button
         onClick={handleLogout}
         className="flex items-center gap-3.5  px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
