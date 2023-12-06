aa5a31219427afdc64fa346fb0616ee848b71de0
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-06 10:56:54 +0700

    Update UI file upload

--- components/FileUpload/index.tsx
+++ components/FileUpload/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Controller, Path, RegisterOptions } from "react-hook-form";
-import { uploadToCloudinary } from "../../utils/uploadToCloudinary";
+import React, { useState } from 'react';
+import { Controller, Path, RegisterOptions } from 'react-hook-form';
+import { uploadToCloudinary } from '../../utils/uploadToCloudinary';
 
 type TPhoto = {
   name: string;
@@ -14,10 +14,7 @@ type TPhoto = {
 const FileUpload = React.forwardRef<HTMLInputElement, TPhoto>((props, ref) => {
   const { name, control, onChange, label, error, rules } = props;
 
-  const handleChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-    onChangeFun: (value: string) => void
-  ) => {
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>, onChangeFun: (value: string) => void) => {
     if (event?.target.files) {
       const file = event.target.files[0];
       const imageUrls = await uploadToCloudinary([file]);
@@ -29,32 +26,32 @@ const FileUpload = React.forwardRef<HTMLInputElement, TPhoto>((props, ref) => {
   };
 
   return (
-    <div className="relative ">
+    <div className="relative">
       <Controller
         rules={rules}
         name={name}
         control={control}
         render={({ field: { onChange } }) => (
-          <div className="block w-full relative my-8">
+          <div className="block w-full relative">
             {label && (
-              <div className="mb-2  flex justify-between text-xs font-bold">
+              <div className="mb-2 text-sm flex justify-between font-medium">
                 <label htmlFor={name}>{label}</label>
               </div>
             )}
             <input
-              className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-medium outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+              className="cursor-pointer rounded-lg border-[1.5px] border-stroke bg-white 
+           font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer 
+           file:border-0 file:border-r file:border-solid file:border-stroke file:py-3
+            file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary
+             active:border-primary disabled:cursor-default disabled:bg-whiter
+               "
               id={name}
               ref={ref}
               onChange={(event) => handleChange(event, onChange)}
-              type={"file"}
-              multiple
+              type={'file'}
               accept="image/jpeg, image/png, video/mp4"
             />
-            {error && (
-              <div className="absolute -bottom-6   text-[15px] text-redCt">
-                {error}
-              </div>
-            )}
+            {error && <div className="absolute -bottom-6 text-[15px] text-redCt">{error}</div>}
           </div>
         )}
       />
