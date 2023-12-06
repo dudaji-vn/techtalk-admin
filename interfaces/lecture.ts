8eb712c24f4e9fa1bb61680f3462910663a3ee4f
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-06 10:59:00 +0700

    Update interface lecture and vocabulary

--- interfaces/lecture.ts
+++ interfaces/lecture.ts
@@ -1,11 +1,21 @@
+import { IFormVocabulary, IVocabulariesByLectureResponse } from './vocabulary';
+
 export interface ILectureItem {
   lectureId: string;
   lectureName: string;
   imgSrc: string;
+  status: 'Draft' | 'Published';
 }
 export interface IFormLecture {
   lectureName: string;
   file?: File | null;
   imgSrc: string;
   lectureId?: string;
+}
+
+export interface IFormLectureAndVocabulary {
+  status: string;
+  lectureName: string;
+  imgSrc: string;
+  listVocabulary: IVocabulariesByLectureResponse[];
 }
