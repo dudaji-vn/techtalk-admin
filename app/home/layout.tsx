bb7575b828f2c577f20208fa816441a2ad9bdc5b
HoQuangLinh <hoquanglinh2710@gmail.com>
2023-12-06 10:56:25 +0700

    Update layout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -8,12 +8,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="bg-white overflow-hidden flex h-screen ml-22">
+    <div className="bg-white100 overflow-hidden flex h-screen ml-20">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="relative flex flex-1 flex-col max-w-screen overflow-hidden">
         <Header />
         <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6  ">{children}</div>
+          <div className="mx-auto max-w-screen-2xl">{children}</div>
         </main>
       </div>
     </div>
