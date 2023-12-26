6745363894052602926f6fda3e573adc66f19dbd
linh <hoquanglinh2710@gmail.com>
2023-12-26 20:34:45 +0700

    Update Typo style

--- components/Typo/index.tsx
+++ components/Typo/index.tsx
@@ -1,33 +1,33 @@
 // TypoComponent.tsx
 
-import React, { FC, HTMLProps } from 'react';
-import { overrideTailwindClasses } from 'tailwind-override';
+import React, { FC, HTMLProps } from "react";
+import { overrideTailwindClasses } from "tailwind-override";
 
 interface TypoProps extends HTMLProps<HTMLParagraphElement> {
-  type: 'xs' | 'small' | 'normal' | 'italic' | 'semi-bold' | '3xl';
+  type: "xs" | "small" | "normal" | "italic" | "semi-bold" | "3xl";
 }
 
-const Typography: FC<TypoProps> = ({ type: typoStyle, children, ...rest }) => {
-  let className = '';
+const Typography: FC<TypoProps> = ({ type: typoStyle, children, className, ...rest }) => {
+  let _className = "";
 
   switch (typoStyle) {
-    case 'xs':
-      className = 'text-secondaryText text-xs';
+    case "xs":
+      _className = "text-secondaryText text-xs";
       break;
-    case 'small':
-      className = 'text-sm';
+    case "small":
+      _className = "text-sm";
       break;
-    case 'normal':
-      className = 'text-base font-normal';
+    case "normal":
+      _className = "text-base font-normal";
       break;
-    case 'italic':
-      className = 'italic';
+    case "italic":
+      _className = "italic";
       break;
-    case 'semi-bold':
-      className = 'font-semibold';
+    case "semi-bold":
+      _className = "font-semibold";
       break;
-    case '3xl':
-      className = 'text-3xl font-semibold';
+    case "3xl":
+      _className = "text-3xl font-semibold";
       break;
 
     default:
@@ -35,7 +35,7 @@ const Typography: FC<TypoProps> = ({ type: typoStyle, children, ...rest }) => {
   }
 
   return (
-    <p className={overrideTailwindClasses(`text-black text-lg ${className}`)} {...rest}>
+    <p className={overrideTailwindClasses(`text-black text-lg ${_className} ${className}`)} {...rest}>
       {children}
     </p>
   );
