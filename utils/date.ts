d8e51f5ec14c45223bf514456593c26908c23deb
linh <hoquanglinh2710@gmail.com>
2024-01-18 16:28:56 +0700

    Add and integrate api certificate page

--- utils/date.ts
+++ utils/date.ts
@@ -2,3 +2,6 @@ import moment from "moment";
 export function formatDate(dateInput: string) {
   return moment(dateInput).format("DD MMM YYYY HH:mm");
 }
+export function formateDateWithoutTime(dateInput: string) {
+  return moment(dateInput).format("DD MMM YYYY ");
+}
