bffc2dfd7a061ef56e8926b36499e0fa10a21ea4
linh <hoquanglinh2710@gmail.com>
2024-01-18 16:26:33 +0700

    Update UI header

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,8 +1,8 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,33 @@
-import { keyStorage } from '@/const/keyStorage';
-import { useRouter } from 'next/navigation';
-import LogoutIcon from '../Icons/LogoutIcon';
-import Typography from '../Typo';
+import { keyStorage } from "@/const/keyStorage";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import LogoutIcon from "../Icons/LogoutIcon";
+import Typography from "../Typo";
+import { ROUTE } from "../../const/path";
+import { useMemo } from "react";
 const Header = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
   const handleLogout = () => {
     localStorage.removeItem(keyStorage.accessToken);
-    router.push('/auth/signin');
+    router.push("/auth/signin");
   };
+  const renderTitle = useMemo(() => {
+    if (pathname.includes(ROUTE.dashboard)) {
+      return "Dashboard";
+    }
+    if (pathname.includes(ROUTE.lectures)) {
+      return "Lectures";
+    }
+    if (pathname.includes(ROUTE.certificates)) {
+      return "User's certificates";
+    }
+
+    return "Admin page";
+  }, [pathname]);
   return (
-    <header className="sticky top-0 z-999 flex justify-between bg-white p-4">
-      <Typography type="semi-bold">Admin page</Typography>
+    <header className="sticky top-0 z-999 flex justify-between bg-mainBg p-4 border border-gray50">
+      <Typography type="semi-bold">{renderTitle}</Typography>
       <button
         onClick={handleLogout}
         className="flex items-center gap-3.5  px-6 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
